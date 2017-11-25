1eedf71d9c91ce5b079ac791e34fcd4226e4f51a
hikalium <hikalium@hikalium.com>
2017-11-26 00:24:51 +0900

    Impl timeline

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('workspotter.sqlite3');
+
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('index', { title: 'Express' });
+  var retv = db.all("select * from freetime", (err, row) => {
+	  if(err){
+		res.status(500).send({ error: 'db fail' });
+		return;
+	  }
+  	res.render('index', { freetimes: row });
+  });
 });
 
 
