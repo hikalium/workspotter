aeb7f223b45583d8862cf2b64eb26cd674a17df1
Yosuke Higuchi <wasapon.dev@gmail.com>
2017-11-25 22:59:51 +0900

    update job_edit

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('workspotter.sqlite3');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    db.all("select * from job", (err, row) => {
+        if(err){
+            res.status(500).send({ error: 'db fail' });
+            return;
+        }
+
+        console.log(row);
+
+        res.render('dashboard', { });
+    });
+
+    //res.render('dashboard', {});
 });
 
 module.exports = router;
