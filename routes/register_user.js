a2de81ae8a06734fdb2375baf1df75865709c797
Yosuke Higuchi <wasapon.dev@gmail.com>
2017-11-25 19:40:09 +0900

    update register_user

--- routes/register_user.js
+++ routes/register_user.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var sqlite3 = require('sqlite3');
+
 var router = express.Router();
+
+var db = new sqlite3.Database('./workspotter.sqlite3');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('register_user');
+});
+
+
+router.post('/', function (req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    db.get('SELECT * FROM user WHERE name = ?', username, function(err, row) {
+        if (!row) {
+            db.run('INSERT INTO USER(name, password, isCompany) VALUES(?, ?, 1)', username, password);
+            console.log("User " + username + " added");
+
+            res.redirect("/register_user");
+        }
+        else {
+            console.log("User " + username + " already exist");
+
+            res.redirect("/register_user");
+        }
+    });
 });
 
 module.exports = router;
