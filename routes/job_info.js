ec0376b28404e04b8331047fc3adac1438c6f0f3
Takaten <takato.y7724@gmail.com>
2017-11-26 12:53:20 +0900

    wip

--- routes/job_info.js
+++ routes/job_info.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+const sqlite3 = require('sqlite3');
+var db = new sqlite3.Database('workspotter.sqlite3');
+
+function showJob(req, res, next) {
+    var jobId = req.query.id;
+    var assign = {};
+    var strSQL = "SELECT * FROM job WHERE id = " + jobId;
+    db.all(strSQL, (err, row) => {
+        if(err){
+            res.status(500).send({ error: 'db fail' });
+            return;
+        }
+        assign['job'] = row[0];
+        console.log(assign);
+        res.render('job_info', assign);
+    });
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    showJob(req, res, next);
 });
 
 module.exports = router;
